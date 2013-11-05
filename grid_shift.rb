367516def9c661b093a54e8982ba70db4203676b
Julie Yaunches <jmyaunch@gmail.com>
2013-11-05 11:42:48 -0500

    collecting tracks based on grid dimensions

--- grid_shift.rb
+++ grid_shift.rb
@@ -1,5 +1,29 @@
 class GridShift
-	def blah
-		puts 'foo'
+	def collect_tracks input, dim_size
+		num_tracks = dim_size/2.0.ceil
+
+		starting_x = 1
+		starting_y = dim_size
+
+		tracks = []
+		num_tracks.times do |index|
+			tracks << pull_track((starting_x + index), (starting_y - index), input)
+			# input = input.select {|k,v| !new_track.keys.include? k }
+			# tracks << new_track
+		end	
+
+
+		return tracks
+	end
+
+	def pull_track x_pos, y_pos, input
+		input.select do |k,v|
+			result = nil
+			if v.include?(x_pos) || v.include?(y_pos)
+				result = input[k] 
+				input.delete k
+			end
+			result
+		end
 	end
 end
--- spec/grid_shift_spec.rb
+++ spec/grid_shift_spec.rb
@@ -2,7 +2,21 @@ require 'spec_helper'
 require File.dirname(__FILE__) + '/../grid_shift'
 
 describe GridShift do
-	it 'should be setup' do
-		GridShift.should == 2
+
+	let(:input) { {'A' => [1,1], 'B' => [1,2], 'C' => [1,3], 'D' => [1,4],
+					'E' => [2,1], 'F' => [2,2], 'G' => [2,3], 'H' => [2,4],
+					'I' => [3,1], 'J' => [3,2], 'K' => [3,3], 'L' => [3,4],
+					'M' => [4,1], 'N' => [4,2], 'O' => [4,3], 'P' => [4,4], } }
+
+	let(:grid_shift) {GridShift.new}					
+	it 'should collect tracks' do
+		tracks = grid_shift.collect_tracks input, 4
+		tracks.size.should == 2
+		tracks[0].should ==  {'A' => [1,1], 'B' => [1,2], 'C' => [1,3], 'D' => [1,4],
+					'E' => [2,1], 'H' => [2,4],
+					'I' => [3,1], 'L' => [3,4],
+					'M' => [4,1], 'N' => [4,2], 'O' => [4,3], 'P' => [4,4], }
+
+		tracks[1].should == { 'F' => [2,2], 'G' => [2,3], 'J' => [3,2], 'K' => [3,3]}					
 	end
 end
